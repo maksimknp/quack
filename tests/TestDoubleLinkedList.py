054f8bd1bf174e24f6c4e6820d43063f64bf4c7b
Maksim Konoplev <maksotron78@gmail.com>
2018-10-08 20:07:35 +0300

    refractoring

--- tests/TestDoubleLinkedList.py
+++ tests/TestDoubleLinkedList.py
@@ -6,62 +6,64 @@ from app.homework2.DoubleLinkedList import DoubleLinkedList
 class TestDoubleLinkedList(unittest.TestCase):
 
     def test_push(self):
-        cities = DoubleLinkedList(Item("Moscow"))
-        cities.push("Minsk")
-        self.assertEqual(cities.last().get_data(), "Minsk")
+        cities = DoubleLinkedList()
+        for city in ['Moscow', 'Minsk']:
+            cities.push(city)
+        self.assertEqual(cities.last().get_data(), 'Minsk')
 
     def test_pop(self):
-        cities = DoubleLinkedList(Item("Moscow"))
-        cities.push("Minsk")
-        self.assertEqual(cities.pop().get_data(), "Minsk")
-        self.assertEqual(cities.pop().get_data(), "Moscow")
+        cities = DoubleLinkedList()
+        for city in ['Moscow', 'Minsk']:
+            cities.push(city)
+        self.assertEqual(cities.pop().get_data(), 'Minsk')
+        self.assertEqual(cities.pop().get_data(), 'Moscow')
         self.assertIsNone(cities.pop())
 
     def test_unshift(self):
-        cities = DoubleLinkedList(Item("Moscow"))
-        cities.unshift("Minsk")
-        self.assertEqual(cities.first().get_data(), "Minsk")
+        cities = DoubleLinkedList(Item('Moscow'))
+        cities.unshift('Minsk')
+        self.assertEqual(cities.first().get_data(), 'Minsk')
 
     def test_shift(self):
-        cities = DoubleLinkedList(Item("Moscow"))
-        cities.push("Minsk")
-        self.assertEqual(cities.shift().get_data(), "Moscow")
-        self.assertEqual(cities.shift().get_data(), "Minsk")
+        cities = DoubleLinkedList()
+        for city in ['Moscow', 'Minsk']:
+            cities.push(city)
+        self.assertEqual(cities.shift().get_data(), 'Moscow')
+        self.assertEqual(cities.shift().get_data(), 'Minsk')
         self.assertIsNone(cities.shift())
 
     def test_len(self):
-        cities = DoubleLinkedList(Item("London"))
+        cities = DoubleLinkedList(Item('London'))
         self.assertEqual(cities.len(), 1)
         cities.pop()
         self.assertEqual(cities.len(), 0)
-        cities.push("Minsk")
+        cities.push('Minsk')
         self.assertEqual(cities.len(), 1)
-        cities.push("Moscow")
+        cities.push('Moscow')
         self.assertEqual(cities.len(), 2)
 
     def test_delete(self):
-        cities = DoubleLinkedList(Item("Moscow"))
-        cities.push("Moscow")
-        cities.push("Moscow")
-        cities.delete("Moscow")
+        cities = DoubleLinkedList()
+        for city in range(3):
+            cities.push('Moscow')
+        cities.delete('Moscow')
         self.assertEqual(cities.len(), 0)
         with self.assertRaises(IOError):
-            cities.delete("Moscow")
-        cities.push("Minsk")
+            cities.delete('Moscow')
+        cities.push('Minsk')
         with self.assertRaises(IOError):
-            cities.delete("Moscow")
+            cities.delete('Moscow')
 
 
     def test_contains(self):
         cities = DoubleLinkedList()
-        self.assertEqual(cities.contains("Moscow"), "List is empty")
-        cities.push("Moscow")
-        self.assertTrue(cities.contains("Moscow"))
+        self.assertEqual(cities.contains('Moscow'), 'List is empty')
+        cities.push('Moscow')
+        self.assertTrue(cities.contains('Moscow'))
 
     def test_first_and_last(self):
         cities = DoubleLinkedList()
-        cities.push("Minsk")
-        cities.push("Moscow")
-        cities.push("London")
-        self.assertEqual(cities.first().get_data(), "Minsk")
-        self.assertEqual(cities.last().get_data(), "London")
+        for city in ['Minsk', 'Moscow', 'London']:
+            cities.push(city)
+        self.assertEqual(cities.first().get_data(), 'Minsk')
+        self.assertEqual(cities.last().get_data(), 'London')
