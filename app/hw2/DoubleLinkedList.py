5ac3e27b0c71857882e0b7319ccafe8bd7fe61d6
Maksim Konoplev <maksotron78@gmail.com>
2018-10-08 12:15:08 +0300

    add homework2 created DoubleLinkedList.class and Item.class created unit test for new classes

---
+++ app/hw2/DoubleLinkedList.py
@@ -0,0 +1,98 @@
+from app.hw2.Item import Item
+
+
+class DoubleLinkedList(object):
+
+    def __init__(self, root=None):
+        self.root = root
+        if self.root is not None:
+            self.size = 1
+        else:
+            self.size = 0
+
+    def unshift(self, data):
+        new_item = Item(data, None, self.root)
+        if self.root is not None:
+            self.root.set_previous_item(new_item)
+        self.root = new_item
+        self.size += 1
+
+    def last(self):
+        last_item = self.root
+        if last_item is not None:
+            while last_item.get_next_item() is not None:
+                last_item = last_item.get_next_item()
+            return last_item
+        else:
+            return None
+
+    def first(self):
+        return self.root
+
+    def push(self, data):
+        last_item = self.last()
+        new_item = Item(data, last_item, None)
+        if last_item is not None:
+            last_item.set_next_item(new_item)
+        else:
+            self.root = new_item
+        self.size += 1
+
+    def len(self):
+        return self.size
+
+    def pop(self):
+        if self.size == 0:
+            return "List is empty"
+        elif self.size == 1:
+            self.root = None
+            self.size = 0
+        else:
+            last_item = self.last()
+            last_item.get_previous_item().set_next_item(None)
+            self.size -= 1
+
+    def shift(self):
+        if self.size == 0:
+            return "List is empty"
+        elif self.size == 1:
+            self.root = None
+            self.size = 0
+        else:
+            self.root = self.root.get_next_item()
+            self.root.set_previous_item(None)
+            self.size -= 1
+
+    def contains(self, data):
+        if self.size == 0:
+            return "List is empty"
+        current_item = self.root
+        while current_item is not None:
+            if current_item.get_data() == data:
+                return True
+            else:
+                current_item = current_item.get_next_item()
+        return False
+
+    def delete(self, data):
+        if self.size == 0:
+            return "List is empty"
+        current_item = self.root
+        while current_item is not None:
+            if current_item.get_data() == data:
+                previous_item = current_item.get_previous_item()
+                next_item = current_item.get_next_item()
+
+                if previous_item is not None:
+                    previous_item.set_next_item(next_item)
+                else:
+                    self.root = current_item.get_next_item()
+
+                if next_item is not None:
+                    next_item.set_previous_item(previous_item)
+
+                self.size -= 1
+                return True
+            else:
+                current_item = current_item.get_next_item()
+        return False
---
+++ app/hw2/TestDoubleLinkedList.py
@@ -0,0 +1,65 @@
+import unittest
+from app.hw2.Item import Item
+from app.hw2.DoubleLinkedList import DoubleLinkedList
+
+
+class TestDoubleLinkedList(unittest.TestCase):
+
+    def test_push(self):
+        list = DoubleLinkedList(Item("Moscow"))
+        list.push("Minsk")
+        self.assertEqual(list.last().get_data(), "Minsk")
+
+    def test_pop(self):
+        list = DoubleLinkedList(Item("Moscow"))
+        list.push("Minsk")
+        list.pop()
+        self.assertEqual(list.last().get_data(), "Moscow")
+        list.pop()
+        self.assertIsNone(list.last())
+        self.assertEqual(list.pop(), "List is empty")
+
+    def test_unshift(self):
+        list = DoubleLinkedList(Item("Moscow"))
+        list.unshift("Minsk")
+        self.assertEqual(list.first().get_data(), "Minsk")
+
+    def test_shift(self):
+        list = DoubleLinkedList(Item("Moscow"))
+        list.push("Minsk")
+        list.shift()
+        self.assertEqual(list.last().get_data(), "Minsk")
+        list.shift()
+        self.assertIsNone(list.last())
+        self.assertEqual(list.shift(), "List is empty")
+
+    def test_len(self):
+        list = DoubleLinkedList(Item("London"))
+        self.assertEqual(list.len(), 1)
+        list.pop()
+        self.assertEqual(list.len(), 0)
+        list.push("Minsk")
+        self.assertEqual(list.len(), 1)
+        list.push("Moscow")
+        self.assertEqual(list.len(), 2)
+
+    def test_delete(self):
+        list = DoubleLinkedList(Item("Moscow"))
+        self.assertTrue(list.delete("Moscow"))
+        self.assertEqual(list.delete("Moscow"), "List is empty")
+        list.push("Minsk")
+        self.assertFalse(list.delete("Moscow"))
+
+    def test_contains(self):
+        list = DoubleLinkedList()
+        self.assertEqual(list.contains("Moscow"), "List is empty")
+        list.push("Moscow")
+        self.assertTrue(list.contains("Moscow"))
+
+    def test_first_and_last(self):
+        list = DoubleLinkedList()
+        list.push("Minsk")
+        list.push("Moscow")
+        list.push("London")
+        self.assertEqual(list.first().get_data(), "Minsk")
+        self.assertEqual(list.last().get_data(), "London")
