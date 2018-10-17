8f1f983a326e920a550b9a6760e1127239b613c9
Maksim Konoplev <maksotron78@gmail.com>
2018-10-18 02:52:14 +0300

    flake8 fixes

--- app/homework3/wsgi.py
+++ app/homework3/wsgi.py
@@ -6,6 +6,6 @@ def application(env, start_resp):
     headers = [('Content-Type', 'application/json')]
     start_resp('200 OK', headers)
     uri = env['wsgi.url_scheme'] + '://' + env['HTTP_HOST'] + env['RAW_URI']
-    resp = json.dumps({'time': '{}'.format(datetime.datetime.now()), 'url': '{}'.format(uri)})
+    date = datetime.datetime.now()
+    resp = json.dumps({'time': '{}'.format(date), 'url': '{}'.format(uri)})
     return [resp.encode('utf-8')]
-
